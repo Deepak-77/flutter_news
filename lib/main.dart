b8f2473bab358399fce849a0baa95cb84fb0f25e
Deepak-77 <deepakupreti7174@gmail.com>
2022-05-10 18:55:06 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_2/screens/main_secreen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+
+//
+// Future<String> getData() async{
+//   print('hello');
+//   await Future.delayed(Duration(seconds: 2));
+//   return 'hello user k cha';
+//   // throw Exception('some error');
+// }
+
+
+void main() async {
+  // getData().then((value) => print(value)).catchError((err) => print(err));
+  //  try{
+  //    final data = await getData();
+  //    print(data);
+  //  }catch (err){
+  //    print('err cha $err');
+  //  }
+
+  runApp(ProviderScope(child: Home()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen()
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
-
